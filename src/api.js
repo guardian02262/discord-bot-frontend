ab29bb768f5ef89dfe326ba738436b06897c411c
NinjaDev <abgjens23@gmail.com>
2021-11-06 11:53:12 +0900

    rebuild for cors error

--- src/api.js
+++ src/api.js
@@ -5,9 +5,11 @@ export const FrontEnd_URL = 'https://guardian-discord-bot-frontend.herokuapp.com
 export const Name = 'Vexer';
 
 export function getAuth() {
+    axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
     return axios.get(`${BackEnd_URL}/api/auth/`, { withCredentials: true })
 }
 
 export function getGuilds() {
+    axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
     return axios.get(`${BackEnd_URL}/api/discord/guilds`, {withCredentials: true })
 }
