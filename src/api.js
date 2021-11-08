537bc20f8b9de3dbe5d573c083c2c1d4aece3d15
guardian0215 <88623862+guardian0215@users.noreply.github.com>
2021-11-07 22:45:48 -0500

    rebuild cause of cors

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 export const BackEnd_URL = 'http://guardian-discord-bot-backend.herokuapp.com';
-export const FrontEnd_URL = 'https://guardian-discord-bot-frontend.herokuapp.com' 
+export const FrontEnd_URL = 'http://guardian-discord-bot-frontend.herokuapp.com' 
 export const Name = 'Vexer';
 
 export function getAuth() {
-    axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
-    // axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+    axios.defaults.headers.post['Content-Type'] ='application/json';
+    axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
     return axios.get(`${BackEnd_URL}/api/auth/`)
 }
 
