3511e078e86b959f6934aff3eafee82d6696a142
guardian0215 <88623862+guardian0215@users.noreply.github.com>
2021-11-07 22:30:07 -0500

    set content

--- src/api.js
+++ src/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-export const BackEnd_URL = 'https://guardian-discord-bot-backend.herokuapp.com';
+export const BackEnd_URL = 'http://guardian-discord-bot-backend.herokuapp.com';
 export const FrontEnd_URL = 'https://guardian-discord-bot-frontend.herokuapp.com' 
 export const Name = 'Vexer';
 
 export function getAuth() {
+    axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
     // axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
     return axios.get(`${BackEnd_URL}/api/auth/`, { withCredentials: true })
 }
